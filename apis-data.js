a09d4244c71aef2386625fedb85a01d765a16feb
Khalid Khan <khalidkhan46572@gmail.com>
2026-06-12 12:50:23 +0500

    Delete apis-data.js

--- apis-data.js
+++
@@ -1,8 +0,0 @@
-// Automatically generated from README.md
-// This file contains pricing data for 1400+ APIs - all updated to price_per_call: 40
-const APIS_DATA = require('./apis-data-full.json').map(api => ({
-  ...api,
-  "price_per_call": 40
-}));
-
-module.exports = APIS_DATA;
